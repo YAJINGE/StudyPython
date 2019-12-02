be22ddda6b412c39a9607eec1dfad20181e16444
YAJINGE <puyajing@yeah.net>
2019-12-02 16:48:48 +0800

    1202

--- chapter_1/string_function.py
+++ chapter_1/string_function.py
@@ -7,3 +7,14 @@ print(str1)
 tuple = ('xiajing','yajing')
 str2 = '*'.join(tuple)
 print(str2)
+
+def sayhello():
+    print('hello word')
+sayhello()
+
+def max(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+print(max(1,5))
---
+++ chapter_2/oo.py
@@ -0,0 +1,2 @@
+class Hello: #创建一个类
+    def __init__(self): #创建一个构造方法
